99b66acdd69aa8e4d5588a89246277b1012af730
theandersonn <theandersonn@gmail.com>
2022-01-31 19:11:36 -0300

    feat: validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { response, Router } from 'express';
+import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
@@ -7,6 +7,10 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category Already exists!' });
+  }
   categoriesRepository.create({ name, description });
   return response.status(201).send();
 });
